0b37024b25e6d41603c2bca7b228b2cdacd36c3a
ilker <ilker@retinasvr.com>
2019-12-13 10:42:53 +0300

    Clean up

--- Src/Web/ViewModels/CurrencyRateViewModel.cs
+++ Src/Web/ViewModels/CurrencyRateViewModel.cs
@@ -7,11 +7,6 @@ namespace Web.ViewModels
 {
     public class CurrencyRateViewModel
     {
-        //quote.LastUpdated
-        //quote.MarketCap
-        //quote.Price
-        //cryptoCurrencyDetail.Name
-        //cryptoCurrencyDetail.Symbol
         public DateTime LastUpdated { get; set; }
         public List<QuoteViewModel> Quotes { get; set; }
         public string CryptoCurrencyName { get; set; }
